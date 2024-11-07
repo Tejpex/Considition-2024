1ad5173e7c8085ecbb89bd0963dcc968f2cf6c6e
Tejpex <equis_angel@hotmail.com>
2024-11-07 20:38:19 +0100

    Finish game-play

--- PyStarterkit/app.py
+++ PyStarterkit/app.py
@@ -5,20 +5,25 @@ import pprint
 import os
 from dotenv import load_dotenv
 
-game_url = "api.considition.com"
-api_key = "d6b11135-fd26-4526-86f3-e445466c9616"
-map_file = "../Map-Gothenburg.json"  # Change map here
-latest_score = 0
+load_dotenv()
 
-breakpoint_environment = 50
-low_breakpoint_capital_difference = 0.2
-high_breakpoint_capital_difference = 0.6
-high_rate = 0.5
-low_rate = 0.055
-rate_increase = 0.08
-rate_decrease = 0.29
-award_number_good = 1500
-award_number_bad = 800
+game_url = "api.considition.com"
+api_key = os.getenv("API_KEY")
+map_file = "../Map-Almhult.json"  # Change map here
+
+breakpoint_environment = 80000
+environment_no = 80000
+low_breakpoint_capital_difference = 0.1
+high_breakpoint_capital_difference = 0.7
+high_rate = 0.8
+low_rate = 0.1
+rate_increase = 1
+rate_decrease = 0.045
+
+low_payback_time = 8
+high_payback_time = 12
+
+start_fund = 500000
 
 responses = []
 
@@ -33,9 +38,29 @@ def calculate_rate(customer_obj):
         new_rate += rate_increase
     if capital_difference > high_breakpoint_capital_difference:
         new_rate -= rate_decrease
-    if customer_obj["personality"] == "Conservative" or customer_obj["personality"] == "Innovative":
+    if customer_obj["personality"] == "Conservative":
         new_rate -= rate_decrease
     return new_rate
+
+
+def calculate_payback_time(customer_obj):
+    if customer_obj["personality"] == "Conservative":
+        time = low_payback_time
+    else:
+        time = high_payback_time
+    return time
+
+
+def approve_customer(customer_obj):
+    if (
+            customer["loan"]["amount"] > start_fund or
+            # customer["capital"] / customer["loan"]["amount"] < low_breakpoint_capital_difference or
+            customer_obj["loan"]["environmentalImpact"] < environment_no
+            # customer_obj["personality"] == "RiskTaker"
+    ):
+        return False
+    else:
+        return True
 
 
 with open(map_file, "r") as file:
@@ -48,30 +73,39 @@ for _ in range(5):
         "Iterations": []
     }
 
+    lent_amount = 0
+
     for customer in map_obj["customers"]:
-        game_input["Proposals"].append({
-            "CustomerName": customer["name"],
-            "MonthsToPayBackLoan": map_obj["gameLengthInMonths"],
-            "YearlyInterestRate": calculate_rate(customer)
-        })
+        if approve_customer(customer):
+            lent_amount += customer["loan"]["amount"]
+            game_input["Proposals"].append({
+                "CustomerName": customer["name"],
+                "MonthsToPayBackLoan": calculate_payback_time(customer),
+                "YearlyInterestRate": calculate_rate(customer)
+            })
 
     award_types = ["IkeaFoodCoupon", "IkeaDeliveryCheck", "IkeaCheck",
                    "GiftCard", "HalfInterestRate", "NoInterestRate"]
-    action_types = ["Skip", "Award"]
+    action_types = ["Award", "Award", "Award"]
 
     for month in range(map_obj["gameLengthInMonths"]):
         customer_actions_dict = {}
         for customer in map_obj['customers']:
-            random_action_type = action_types[random.randint(0, len(action_types) - 1)]
+            if month % 2 == 0:
+                random_action_type = action_types[random.randint(0, len(action_types) - 1)]
+            else:
+                random_action_type = "Skip"
             random_award = "None" if random_action_type == "Skip" else random.choice(award_types)
-            customer_actions_dict[customer["name"]] = {
-                "Type": random_action_type,
-                "Award": random_award
-            }
+            if approve_customer(customer):
+                customer_actions_dict[customer["name"]] = {
+                    "Type": random_action_type,
+                    "Award": random_award
+                }
         game_input["Iterations"].append(customer_actions_dict)
 
     # Preview game-input
     # pprint.pprint(game_input["Proposals"])
+    # print(lent_amount)
 
     conn = http.client.HTTPSConnection(game_url)
     headers = {
@@ -91,9 +125,7 @@ for _ in range(5):
 
     conn.close()
 
-scores = []
-for response in responses:
-    scores.append(response["score"]["totalScore"])
+scores = [response["score"]["totalScore"] for response in responses]
 responses = []
 
 average_score = sum(scores) / len(scores)
