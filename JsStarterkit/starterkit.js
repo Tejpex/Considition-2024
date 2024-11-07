1a356d71e713307052bffeef5a1e5266e148c0ee
Tejpex <equis_angel@hotmail.com>
2024-11-07 14:30:49 +0100

    Remove C#

--- JsStarterkit/starterkit.js
+++ JsStarterkit/starterkit.js
@@ -1,28 +1,28 @@
-const https = require("https");
-var fs = require("fs");
+import { request } from "https"
+import { readFileSync } from "fs"
 
-const gameUrl = "api.considition.com";
-const apiKey = "ENTER-YOUR-API-KEY-HERE"; // API-key is sent in mail inbox
-const mapFile = "Map-Gothenburg.json"; // Change map here
+const gameUrl = "api.considition.com"
+const apiKey = "d6b11135-fd26-4526-86f3-e445466c9616"
+const mapFile = "Map-Gothenburg.json"
 
-var obj = JSON.parse(fs.readFileSync(mapFile, "utf8"));
+var obj = JSON.parse(readFileSync(mapFile, "utf8"))
 
 const gameInput = {
-    MapName: "Gothenburg",
-    Proposals: [],
-    Iterations: [],
-};
-
-for (let customer of obj.customers) {
-    gameInput.Proposals.push({
-        CustomerName: customer.name,
-        MonthsToPayBackLoan: obj.gameLengthInMonths,
-        YearlyInterestRate: 0.05,
-    });
+  MapName: "Gothenburg",
+  Proposals: [],
+  Iterations: [],
 }
 
-const actionTypes = ["Award", "Skip"];
-const awardTypes = ["IkeaFoodCoupon", "IkeaDeliveryCheck", "IkeaCheck", "GiftCard", "HalfInterestRate", "NoInterestRate"];
+for (let customer of obj.customers) {
+  gameInput.Proposals.push({
+      CustomerName: customer.name,
+      MonthsToPayBackLoan: obj.gameLengthInMonths,
+      YearlyInterestRate: 0.05,
+  })
+}
+
+const actionTypes = ["Award", "Skip"]
+const awardTypes = ["IkeaFoodCoupon", "IkeaDeliveryCheck", "IkeaCheck", "GiftCard", "HalfInterestRate", "NoInterestRate"]
 
 for (let i = 0; i < obj.gameLengthInMonths; i++) {
     const customerActionsDict = {};
@@ -55,7 +55,7 @@ const options = {
     },
 };
 
-const req = https.request(options, (res) => {
+const req = request(options, (res) => {
     let body = "";
     res.on("data", (chunk) => {
         body += chunk;
--- PyStarterkit/app.py
+++ PyStarterkit/app.py
@@ -2,60 +2,141 @@ import http.client
 import json
 import random
 import pprint
+import pandas
+import time
 
 game_url = "api.considition.com"
-api_key = "ENTER API KEY HERE" # API-key is sent in mail inbox
-map_file = "Map-Gothenburg.json" # Change map here
+api_key = "d6b11135-fd26-4526-86f3-e445466c9616"
+map_file = "Map-Gothenburg.json"  # Change map here
+latest_score = 0
+
+breakpoint_environment = 50
+low_breakpoint_capital_difference = 0.2
+high_breakpoint_capital_difference = 0.6
+high_rate = 0.5
+low_rate = 0.055
+rate_increase = 0.08
+rate_decrease = 0.015
+award_number_good = 1500
+award_number_bad = 800
+gordons_rate = 0.02
+glenns_rate = 0.57
+kims_rate = 0.01
+responses = []
 
 with open(map_file, "r") as file:
-    obj = json.load(file)
+    map_obj = json.load(file)
 
-game_input = {
-    "MapName": "Gothenburg",
-    "Proposals": [],
-    "Iterations": []
-}
+data_to_save = [[
+        "breakpoint_environment",
+        "high_rate",
+        "low_rate",
+        "Glenn",
+        "environmentalImpact",
+        "happynessScore",
+        "totalProfit"
+        "totalScore"]]
 
-for customer in obj["customers"]:
-    game_input["Proposals"].append({
-        "CustomerName": customer["name"],
-        "MonthsToPayBackLoan": obj["gameLengthInMonths"],
-        "YearlyInterestRate": 0.05
-    })
+for _ in range(100):
+    time.sleep(0.8)
+    for _ in range(5):
+        game_input = {
+            "MapName": map_obj["name"],
+            "Proposals": [],
+            "Iterations": []
+        }
 
+        for customer in map_obj["customers"]:
+            if customer["loan"]["environmentalImpact"] < breakpoint_environment:
+                rate = high_rate
+            else:
+                rate = low_rate
+            if customer["capital"] / customer["loan"]["amount"] < low_breakpoint_capital_difference:
+                rate += rate_increase
+            if customer["capital"] / customer["loan"]["amount"] > high_breakpoint_capital_difference:
+                rate -= rate_decrease
+            if customer["personality"] == "Conservative" or customer["personality"] == "Innovative":
+                rate -= rate_decrease
+            if customer["name"] == "Gordon":
+                rate = gordons_rate
+            if customer["name"] == "Glenn":
+                rate = glenns_rate
+            if customer["name"] == "Kim":
+                rate = kims_rate
+            game_input["Proposals"].append({
+                "CustomerName": customer["name"],
+                "MonthsToPayBackLoan": map_obj["gameLengthInMonths"],
+                "YearlyInterestRate": rate
+            })
 
-action_types = ["Award", "Skip"]
-award_types = ["IkeaFoodCoupon", "IkeaDeliveryCheck", "IkeaCheck", "GiftCard", "HalfInterestRate", "NoInterestRate"]
+        award_types = ["IkeaFoodCoupon", "IkeaDeliveryCheck", "IkeaCheck",
+                       "GiftCard", "HalfInterestRate", "NoInterestRate"]
 
-for index in range(obj["gameLengthInMonths"]):
-  customer_actions_dict = {}
-  for customer in obj['customers']:
-    random_index = random.randint(0, len(action_types) - 1)
-    random_type = action_types[random_index];
-    random_award = "None" if random_type == "Skip" else random.choice(award_types)
-    customer_actions_dict[customer["name"]] = {
-        "Type": random_type,
-        "Award": random_award
-    }
-  game_input["Iterations"].append(customer_actions_dict)
+        for month in range(map_obj["gameLengthInMonths"]):
+            customer_actions_dict = {}
+            for customer in map_obj['customers']:
+                if customer["personality"] == "Conservative" or customer["personality"] == "Practical":
+                    action_types = ["Skip"]
+                    for i in range(award_number_good):
+                        action_types.append("Award")
+                else:
+                    action_types = ["Skip"]
+                    for i in range(award_number_bad):
+                        action_types.append("Award")
+                random_action_type = action_types[random.randint(0, len(action_types) - 1)]
+                random_award = "None" if random_action_type == "Skip" else random.choice(award_types)
+                customer_actions_dict[customer["name"]] = {
+                    "Type": random_action_type,
+                    "Award": random_award
+                }
+            game_input["Iterations"].append(customer_actions_dict)
 
+        # Preview game-input
+        # pprint.pprint(game_input["Proposals"])
 
-# Uncomment this to preview game
-# pprint.pprint(game_input)
+        conn = http.client.HTTPSConnection(game_url)
+        headers = {
+            "Content-Type": "application/json",
+            "x-api-key": api_key
+        }
 
-conn = http.client.HTTPSConnection(game_url)
-headers = {
-    "Content-Type": "application/json",
-    "x-api-key": api_key
-}
+        conn.request("POST", "/game", json.dumps(game_input), headers)
+        response = conn.getresponse()
+        res_body = response.read().decode()
 
-conn.request("POST", "/game", json.dumps(game_input), headers)
-response = conn.getresponse()
-body = response.read().decode()
+        if response.status == 200:
+            pprint.pprint(json.loads(res_body))
+            responses.append(json.loads(res_body))
+        else:
+            print(f"Error: {response.status} - {res_body}")
 
-if response.status == 200:
-    pprint.pprint(json.loads(body))
-else:
-    print(f"Error: {response.status} - {body}")
+        conn.close()
 
-conn.close()
+    scores = []
+    for response in responses:
+        scores.append(response["score"]["totalScore"])
+        data_to_save.append([
+            breakpoint_environment,
+            high_rate,
+            low_rate,
+            kims_rate,
+            response["score"]["environmentalImpact"],
+            response["score"]["happynessScore"],
+            response["score"]["totalProfit"],
+            response["score"]["totalScore"]],
+        )
+    responses = []
+
+    average_score = sum(scores) / len(scores)
+    print(average_score)
+
+    if average_score > latest_score or latest_score - average_score < 1000:
+        kims_rate += 0.01
+        latest_score = average_score
+    elif average_score < latest_score and latest_score - average_score > 1000:
+        if kims_rate >= 0.01:
+            kims_rate -= 0.01
+        latest_score = average_score
+
+csv_data = pandas.DataFrame(data_to_save)
+csv_data.to_csv("Data.csv")
